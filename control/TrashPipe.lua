c7997949c8930f5761f1f857fea75313196bf8e3
yukicode <yolandaapply@gmail.com>
2016-05-24 22:28:55 -0700

    Fixed trash pipe so that it works for oil

--- control/TrashPipe.lua
+++ control/TrashPipe.lua
@@ -3,7 +3,7 @@ require("Common")
 function OnTrashPipeEntityTick(_EntityInfo)
     local fluid = _EntityInfo.entity
     if (fluid ~= nil and fluid.valid) then
-        if (fluid.fluidbox[1] ~= nil and fluid.fluidbox[1].amount == 10) then
+        if (fluid.fluidbox[1] ~= nil and fluid.fluidbox[1].amount >= 9.99) then
             fluid.fluidbox[1] = nil
         end
     end
